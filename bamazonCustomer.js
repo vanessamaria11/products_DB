c17d6b5fc193b258310db7a2183bc0c992eed36b
vanessamaria11 <vanessamariaotto@gmail.com>
2017-07-07 08:51:17 -0400

    working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -36,96 +36,119 @@ var start = function(){
 };
 start();
 
-function displayItems(){
+var displayItems = function(){
 	connection.query('SELECT * FROM products', function(error, rows)
 	{
 		if (error) throw error;
-		console.log(rows);
+		// console.log(rows);
 		// var table = "/t";
 		// console.log("ItemID\tProduct Name\tDepartment Name\tPrice\tAmount In Stock")
 		// console.log("--------------------------------------------------------");
 		// console.log("database connected:\n");
-		// // console.log(rows);
-		// for (var i = 0; i < rows.length; i++){
-		// console.log(rows[i].item_id + table + rows[i].product_name + table + rows[i].dept_name +
-  //       table + rows[i].price + table + rows[i].stock_quant);
-		// }
-		// inquirer.prompt({
-		// 	name:"ask",
-		// 	type:"list",
-		// 	message:"would you like to purchase an item?",
-		// 	choices:["yes","no"]
-		// }).then(function(answers){
-		// 	if(answers.ask === "yes"){
-		// 		selection();
-		// 	}
-		// 	else(console.log("Alright..."))
-		// });
+		// console.log(rows);
+		for (var i = 0; i < rows.length; i++){
+		console.log(rows[i].item_id + " | " + rows[i].product_name + " | " + rows[i].dept_name +
+        " | " + rows[i].price + " | " + rows[i].stock_quant);
+		}
+		inquirer.prompt({
+			name:"ask",
+			type:"list",
+			message:"would you like to purchase an item?",
+			choices:["yes","no"]
+		}).then(function(answers){
+			if(answers.ask === "yes"){
+				selectionPrompt(rows);
+			}
+			else(console.log("Alright..."))
+		});
 	});
 }
 
-function selection(){
+var selectionPrompt = function(rows){
+	// var productmatch = false;
+
 		inquirer.prompt([
 			{
 				name:"which",
 				type:"input",
 				message: "what would you like to purchase?"
-			},
-			{
-				name: "dept",
-				type: "input",
-				message: "What department is this product in?"
-			},
-			{
-				name: "price",
-				type: "input",
-				message: "What is the price of the item?"
-			},
-
-			{
-				name:"amount",
-				type:"input",
-				message:"How many of this item would you like to purchase?"
-		}]).then(function(answer)
+			}]).then(function(answer)
 				{
-					connection.query("INSERT INTO products(product_name, dept_name, price, stock_quant) VALUES('"+ answer.which +"','" + answer.dept + "'," + answer.price + "," + answer.quantity + ");",
-						function(error, res){
-							if (error){
-								throw error;
-							}
-							console.log("Item added!");
-							displayItems();
-						});
+					var productmatch = false;
+
+					for (var i = 0; i < rows.length; i++){
+						if (rows[i].product_name === answer.which){
+							productmatch = true;
+							var product = answer.which;
+							var id = i;
+							inquirer.prompt([{
+								name:"amount",
+								type:"input",
+								message:"How many of this item would you like to purchase?"
+							}]).then(function(answer){
+								if((rows[id].stock_quant - answer.amount) > 0){
+									connection.query(
+										"UPDATE products SET stock_quant='" + (rows[id].stock_quant - answer.amount) + "' WHERE product_name='" + product + "'",
+										function(error, response){
+											if (error){
+												throw error;
+											}
+											console.log("You've successfully purchased this product.");
+
+											start();
+										});
+								}
+								else {
+									console.log("Entry not valid");
+									selectionPrompt(rows);
+								}
+							});
+						}
+						if(answer.which === "Q" || answer.which === "q"){
+							process.exit();
+						}
+					};
 					});
-				}
-		function addQuantity(){
-			inquirer.prompt([
-			{
-				type: "input",
-				name: "productName",
-				message: "What product are you updating?"
-			},
-			{
-				type: "input",
-				name: "new",
-				message: "How much of more of this item do you want?"
-			}
-			]).then(function(val){
-				connection.query(
-					"UPDATE products SET stock_quant = stock_quant" + val.newQuantity + " WHERE product_name ='" +val.productName + "'",
-					function(error, results){
-						if(error){
-							throw error;
-						}
-						if (results.affectedRows ===0){
-							console.log("That item is not available.");
-							displayItems();
-						}
-						else {
-							console.log("You have successfully added items!");
-							displayItems();
-						}
-					})
-			})
-		}
-		//product_name dept_name price stock_quant
+					};
+		// 			connection.query("INSERT INTO products(product_name, dept_name, price, stock_quant) VALUES('" + answer.which +"," + answer.dept + "," + answer.price + "," + answer.amount + ");",
+
+		// 				function(error, res){
+		// 					if (error){
+		// 						throw error;
+		// 					}
+		// 					console.log("Item added!");
+		// 					displayItems();
+		// 				});
+		// 			});
+		// 		}
+		// function addQuantity(){
+		// 	inquirer.prompt([
+		// 	{
+		// 		type: "input",
+		// 		name: "productName",
+		// 		message: "What product are you updating?"
+		// 	},
+		// 	{
+		// 		type: "input",
+		// 		name: "new",
+		// 		message: "How much of more of this item do you want?"
+		// 	}
+		// 	]).then(function(val){
+		// 		connection.query(
+		// 			"UPDATE products SET stock_quant = stock_quant" + val.newQuantity + " WHERE product_name ='" +val.productName + "'",
+		// 			function(error, results){
+		// 				if(error){
+		// 					throw error;
+		// 				}
+		// 				if (results.affectedRows ===0){
+		// 					console.log("That item is not available.");
+		// 					displayItems();
+		// 				}
+		// 				else {
+		// 					console.log("You have successfully added items!");
+		// 					displayItems();
+		// 				}
+		// 			})
+		// 	})
+		// }
+		// //product_name dept_name price stock_quant
